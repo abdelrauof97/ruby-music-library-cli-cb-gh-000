acf9fd998a1efb1debc4d65c5b995fad6250e5f4
abdelrauof mohammed <abdelrauof97@gmail.com>
2018-01-27 21:31:50 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -28,7 +28,8 @@ class MusicLibraryController
           list_songs
         when 'list artists'
           list_artists
-        when 
+        when 'list genres'
+          list_genres
         
       end
     end
