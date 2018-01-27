758421b9573a1f49827989c47d874754ca9ab4bb
abdelrauof mohammed <abdelrauof97@gmail.com>
2018-01-27 21:04:00 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -62,7 +62,7 @@ class MusicLibraryController
   def play_song
     puts "Which song number would you like to play?"
     user_input = gets.strip
-    if (1..Song.all.size).include?(user_input)
+    if (1..Song.all.length).include?(user_input)
       puts "Playing #{song.name} by #{song.artist.name}"
     end
   end
